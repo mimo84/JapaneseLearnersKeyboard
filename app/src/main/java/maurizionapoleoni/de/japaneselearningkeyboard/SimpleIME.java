fc3759226f807a0bf92854a47a05530705666195
Maurizio Napoleoni <napy84@gmail.com>
2015-10-22 20:55:42 +1000

    Initial commit

---
+++ app/src/main/java/maurizionapoleoni/de/japaneselearningkeyboard/SimpleIME.java
@@ -0,0 +1,73 @@
+package maurizionapoleoni.de.japaneselearningkeyboard;
+
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+
+public class SimpleIME extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+    private KeyboardView kv;
+    private Keyboard keyboard;
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+        InputConnection ic = getCurrentInputConnection();
+
+        switch (primaryCode) {
+            case Keyboard.KEYCODE_DELETE:
+                ic.deleteSurroundingText(1, 0);
+                break;
+            case Keyboard.KEYCODE_SHIFT:
+                kv.invalidateAllKeys();
+                break;
+            case Keyboard.KEYCODE_DONE:
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                break;
+            default:
+                char code = (char) primaryCode;
+                ic.commitText(String.valueOf(code), 1);
+
+        }
+    }
+
+    @Override
+    public void onPress(int primaryCode) {
+    }
+
+    @Override
+    public void onRelease(int primaryCode) {
+    }
+
+    @Override
+    public void onText(CharSequence text) {
+    }
+
+    @Override
+    public void swipeDown() {
+    }
+
+    @Override
+    public void swipeLeft() {
+    }
+
+    @Override
+    public void swipeRight() {
+    }
+
+    @Override
+    public void swipeUp() {
+    }
+
+    @Override
+    public View onCreateInputView() {
+        kv = (KeyboardView)getLayoutInflater().inflate(R.layout.keyboard, null);
+        keyboard = new Keyboard(this, R.xml.hiragana);
+        kv.setKeyboard(keyboard);
+        kv.setOnKeyboardActionListener(this);
+        return kv;
+    }
+
+}
